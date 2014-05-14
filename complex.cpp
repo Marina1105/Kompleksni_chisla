1f743e5f417d368dabd27fa0576e285efc368164
Andrianochka <Andriana-zima@yandex.ru>
2014-05-14 08:58:10 +0300

    Update complex.cpp
    dodala mnogenia y vigliadi drugnioi funcii

--- complex.cpp
+++ complex.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
 #include <iostream>
+#include <math.h>
+
 
 using namespace std;
 class complex
@@ -10,13 +12,12 @@ double im;
 double Arg;
 double Modul;
 public:
-complex(double RE, double IM);
+complex(double RE);
 ~complex();
-complex operator-();// çðîáèòè
+complex operator-(complex &a);// çðîáèòè
 complex operator+(complex &b); // ïðàâèëüíî
 friend complex operator* (complex &a, complex &b);// ìíîæåííÿ
-complex operator^ (complex &c);// ìíîæåííÿ ñêàëÿðíèé äîáóòîê
-complex operator% (complex &c);// ìíîæåííÿ
+
 complex operator/ (complex &b);//ä³ëåííÿ ïåðåèìåíîâàëà 
 friend complex operator^(complex &a, double x);//ñòåï³íü ïðàâèëüíî
 double getRe();
@@ -34,18 +35,28 @@ complex operator=(complex &b)
 re=b.re;
 im=b.im;
 }
+friend complex operator* (complex &a, complex &b)
+   {
+      complex c(0,0);
+    c.re = (a.re * b.re) - (a.im * b.im);
+    c.im = (a.re * b.im) + (a.im * b.re);
+
+    return c;
+}
 };
-complex::complex(double RE, double IM)
+complex::complex(double RE)
 {
 re=RE;
-im=IM;
+im=0;
 }
 complex::~complex()
 {
 }
-complex complex::operator-()
+complex complex::operator-(complex &a)
 {
-complex x(-re, -im);
+complex x(re, im);
+x.re=re-a.re;
+x.im=re-a.im;
 return x;
 }
 void complex:: show()
@@ -65,22 +76,18 @@ x.im = im + b.im;
 return x;
 
 }
-complex operator* (complex &a,complex &b)
-{
-complex c(0,0);
-c.re=a.re*a.re-b.im*b.im;
-c.im=a.re*b.im+b.re*a.im;
-return c;
-}
+
 
 int main(int argc, char *argv[])
 { complex a(8,1);
 complex b(5,1);
-(-a).show();
+complex c (0,0);
 cout<<endl;
 (a).show();
 cout<<endl;
 (b).show();
+c =(a*b);
+c.show();
 
 system("PAUSE");
 return EXIT_SUCCESS;
